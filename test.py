64c586314ee4009dd0162607f50ccc3cfd6a9ebe
Famidore <lokoroko456@gmail.com>
2023-12-21 12:37:54 +0100

    run

--- test.py
+++ test.py
@@ -12,3 +12,7 @@ def index():
 @app.route('/favicon.ico')
 def favicon():
 	return send_from_directory(os.path.join(app.root_path, 'static'), 'squid.ico', mimetype='image/vnd.microsoft.icon')
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0', port=8000)
