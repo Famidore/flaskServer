1dcfd37add0313d5449a0d48bb4607ca25cf0ce4
Famidore <lokoroko456@gmail.com>
2023-12-21 21:23:07 +0100

    navigation

--- test.py
+++ test.py
@@ -1,18 +1,28 @@
-from flask import Flask
-from flask import send_from_directory
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
+from datetime import datetime
 import os
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/favicon.ico')
+
+@app.route("/favicon.ico")
 def favicon():
-	return send_from_directory(os.path.join(app.root_path, 'static'), 'squid.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "squid.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
 
 
-if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=8000)
+@app.route("/time", methods=["GET", "POST"])
+def time():
+    return render_template("time.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
