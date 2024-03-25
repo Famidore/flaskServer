f91abade9c89b918f636a784d600d7ee7f986dd1
Maciej Kowalski <sirkowi3@gmail.com>
2024-03-25 17:49:27 +0000

    Database develop (#1)
    * Moved to SQLAlchemy, added registering users to database
    
    * Moved to SQLAlchemy, added registering users to database

--- src/auth/models.py
+++
@@ -1,8 +0,0 @@
-from dbs import db
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
