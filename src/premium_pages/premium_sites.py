752815ed81d9e7e15a49681a5fa6a46442c7dc93
Famidore <lokoroko456@gmail.com>
2024-04-20 15:06:15 +0200

    search for and add categories

--- src/premium_pages/premium_sites.py
+++ src/premium_pages/premium_sites.py
@@ -1,5 +1,5 @@
-from quart import render_template, Blueprint, redirect, url_for
-from quart_auth import login_required, current_user, Unauthorized
+from quart import render_template, Blueprint, redirect, url_for, request
+from quart_auth import login_required, Unauthorized
 from src.premium_pages.premium_scrapers.movies_premium import get_movies_premium
 from src.premium_pages.premium_scrapers.reddit_premium import get_reddit_premium
 from src.premium_pages.premium_scrapers.wykop_premium import get_wykop_premium
@@ -17,12 +17,27 @@ async def premium_main():
     return await render_template("premium/premium_hub.html")
 
 
-@premium.route("/hub/configure", methods=["POST", "GET"])
+@premium.route("/hub/configure", methods=["POST"])
 @login_required
 async def premium_config():
     userCategories = obtain_key(
         "src/premium_pages/premium_scrapers/tempCategories.json", reneder_whole=True
     )
+
+    if request.method == "POST":
+        data_form = await request.form
+        if list(data_form.to_dict().keys())[0] == "user_category_list":
+            data = list(data_form.to_dict().values())[0]
+            if data in userCategories[1][0]:
+                print(data)
+                # modify dbs
+            else:
+                print("field not in dataset")
+        elif list(data_form.to_dict().keys())[0] == "category_list":
+            data = list(data_form.to_dict().values())[0]
+            print("category_list")
+            # add to user categories dbs
+
     return await render_template(
         "premium/premium_config.html",
         allCategories=get_categories(10),
