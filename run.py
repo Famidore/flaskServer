c10fcd44c5a5cab0ba6b8854feedb46b8e749c24
Dominik <gildominik@gmail.com>
2024-05-24 11:24:04 +0200

    Michal frontend (#20)
    * Add a way to reference a different stylesheet from each .html file
    
    * Create basic login tamplate, classes for login-form div and inputs.
    
    * Create login.html tamplate, login_page.css stylesheet. Not full responsive version.
    
    * Rebuild signup.html template, create signup.css and correct login.css to be similar to signup.css
    
    * Create index template and stylesheet, correct login html/css to more responsive
    
    * create and style unauth, create signup_response, register endpoint in auth.py
    
    ---------
    
    Co-authored-by: kacper651 <kacper.wroblewski651@gmail.com>
    Co-authored-by: Buniotoja <m.buniowski@interia.pl>
    Co-authored-by: Paweł <lokoroko456@gmail.com>

--- run.py
+++ run.py
@@ -1,20 +1,12 @@
 import quart_flask_patch
 from quart import Quart
 from quart_auth import QuartAuth
-from werkzeug.local import LocalProxy
 from src.pages.posts_db.models import db
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from src.pages.posts_db.adding_posts import (
-    add_wykop_to_db,
-    add_movies_to_db,
-    add_youtube_to_db,
-    add_reddit_to_db,
-    initialize_platforms,
-)
+from src.pages.posts_db.adding_posts import add_wykop_to_db, add_movies_to_db, add_youtube_to_db, add_reddit_to_db
 from src.utils import obtain_key
-import os
-import json
+
 
 
 auth_manager = QuartAuth()
@@ -54,23 +46,21 @@ def setup_app():
     from src.trends import trending as trending_blueprint
     from src.premium_pages.premium_sites import premium as premium_blueprint
     from src.user_profile.profile import user_profile as profile_blueprint
-    from src.admin_hub.admin_index import admin_hub as admin_blueprint
 
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
     app.register_blueprint(trending_blueprint)
     app.register_blueprint(premium_blueprint)
     app.register_blueprint(profile_blueprint)
-    app.register_blueprint(admin_blueprint)
 
     return app
 
 
 application = setup_app()
 
-# -------database-------#
-scheduler = AsyncIOScheduler()
 
+#-------database-------#
+scheduler = AsyncIOScheduler()
 
 async def add_posts_to_db():
     await add_wykop_to_db()
@@ -83,13 +73,11 @@ async def add_posts_to_db():
 async def create_db_tables():
     async with application.app_context():
         db.create_all()
-    initialize_platforms()
     await add_posts_to_db()
 
-
-scheduler.add_job(add_posts_to_db, "interval", hours=2)
+scheduler.add_job(add_posts_to_db, 'interval', hours=2)
 scheduler.start()
-# -------end database-------#
+#-------end database-------#
 
 if __name__ == "__main__":
     asyncio.run(application.run_task(debug=True, host="0.0.0.0", port=5050))
--- src/auth/auth_db/auth_db.py
+++ src/auth/auth_db/auth_db.py
@@ -1,4 +1,4 @@
-from models import db, User
+from .models import db, User
 
 
 def register_user(name, email, password):
