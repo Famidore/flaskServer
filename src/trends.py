58af3af3881a7f970809cf7b73928b169dfff585
Dominik <gildominik@gmail.com>
2024-05-24 11:15:24 +0200

    Auth & threads fix (#18)
    * hypercorn (#15)
    
    * Blueprints fro auth
    
    * login form setup
    
    * requirements update
    
    * Database develop (#1)
    
    * Moved to SQLAlchemy, added registering users to database
    
    * Moved to SQLAlchemy, added registering users to database
    
    * newsletter setup
    
    * Add navbar.html and seperate it from index.html
    
    * housekeeping and including new navbar into pages
    
    * add logo, base.html and rework index and navbar to bootstrap
    
    * Refactor templates to use base.html
    
    * Refactor HTML templates for Reddit and Wykop pages
    
    * Update styles and navbar to fit discussed colors
    
    * Update page titles and navbar links
    
    * Remove background color styling from index.html
    
    * Added Post model and adding movies to database
    
    * premium content setup
    
    * auth progress
    
    * auth progress
    
    * I am a dust gently touched by the Spring wind gusts
    
    * credencials setup
    
    * credentials secured
    
    * fix movies bug
    
    * Time to embrace the chaos and rewrite history... or cry into my keyboard. 😭
    
    * Added adding posts to database <work in progress>
    
    * Kacper frontend test (#6)
    
    * Add navbar.html and seperate it from index.html
    
    * housekeeping and including new navbar into pages
    
    * add logo, base.html and rework index and navbar to bootstrap
    
    * Refactor templates to use base.html
    
    * Refactor HTML templates for Reddit and Wykop pages
    
    * Update styles and navbar to fit discussed colors
    
    * Update page titles and navbar links
    
    * Remove background color styling from index.html
    
    * Connected to <local> PostgreSQL database, now app is creating posts records on startup
    
    * Now posts are added to db every 2 hours
    
    * Getfromdb (#8)
    
    * Add a way to reference a different stylesheet from each .html file
    
    * Fix stupid JetBrains formatting and ignore .idea file
    
    * Delete /time and beutify files afer pycharm fuckup ✨
    
    * Switched database to an online hosted one
    
    * Getting posts from database,I don't use threads tho
    
    * Update run.py
    
    ---------
    
    Co-authored-by: kacper651 <kacper.wroblewski651@gmail.com>
    Co-authored-by: km46563 <sirkowi3@gmail.com>
    
    * Premium content (#9)
    
    * admin credencials && updating CONFIG file from web interface
    
    * categories searching with autofill
    
    * search for and add categories
    
    * youtube trends by category
    
    * integrated movies + series scrapper
    
    * Update requirements.txt
    
    * Azure hypercorn (#14)
    
    * add hypercorn
    
    * automatic config generation
    
    ---------
    
    Co-authored-by: Maciej Kowalski <sirkowi3@gmail.com>
    Co-authored-by: kacper651 <kacper.wroblewski651@gmail.com>
    Co-authored-by: Dominik Gil <gildominik@gmail.com>
    Co-authored-by: Kacper Wróblewski <85016688+kacper651@users.noreply.github.com>
    
    * Update requirements.txt --hotfix
    
    * fixed problems with cookies
    
    ---------
    
    Co-authored-by: Paweł <lokoroko456@gmail.com>
    Co-authored-by: Maciej Kowalski <sirkowi3@gmail.com>
    Co-authored-by: kacper651 <kacper.wroblewski651@gmail.com>
    Co-authored-by: Kacper Wróblewski <85016688+kacper651@users.noreply.github.com>

--- src/trends.py
+++ src/trends.py
@@ -13,11 +13,16 @@ yt_api_key = obtain_key(mode="youtube_key")
 
 @trending.route("/trends", methods=["GET"])
 async def trends():
-    '''
-    t_yt = threadReturn(target=get_posts('youtube', 10))
-    t_rd = threadReturn(target=get_posts('reddit', 10))
-    t_wykop = threadReturn(target=get_posts('wykop', 10))
-    t_movies = threadReturn(target=get_posts('filmweb', 10))
+    """
+    youtube_name = "youtube"
+    wykop_name = "wykop"
+    filmweb_name = "filmweb"
+    reddit_name = "reddit"
+
+    t_yt = threadReturn(target=get_posts, args=(youtube_name,))
+    t_rd = threadReturn(target=get_posts, args=(reddit_name,))
+    t_wykop = threadReturn(target=get_posts, args=(wykop_name,))
+    t_movies = threadReturn(target=get_posts, args=(filmweb_name,))
 
     t_yt.start()
     t_rd.start()
@@ -27,11 +32,12 @@ async def trends():
     movies_list, movies_posters, movies_links = t_movies.join()
     yt_titles, yt_imgs, yt_urls = t_yt.join()
     rd_titles, rd_src, rd_link, rd_img, rd_descs = t_rd.join()
-    '''
-    movies_list, movies_posters, movies_links = get_posts('filmweb', 10)
-    yt_titles, yt_imgs, yt_urls = get_posts('youtube', 10)
-    rd_titles, rd_src, rd_link, rd_img, rd_descs = get_posts('reddit', 10)
-    wykop_titles, wykop_imgs, wykop_links = get_posts('wykop', 10)
+    wykop_titles, wykop_imgs, wykop_links = t_wykop.join()
+    """
+    movies_list, movies_posters, movies_links = get_posts("filmweb", 10)
+    yt_titles, yt_imgs, yt_urls = get_posts("youtube", 10)
+    rd_titles, rd_src, rd_link, rd_img, rd_descs = get_posts("reddit", 10)
+    wykop_titles, wykop_imgs, wykop_links = get_posts("wykop", 10)
 
     return await render_template(
         "trends.html",
